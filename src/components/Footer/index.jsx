435aecfb4539abcdb413b7cb3efaeeb3e15b0532
David Felix <davidfelix1997@hotmail.com>
2020-03-22 15:58:31 +0000

    i was trying to remove extraneous files

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './style.scss';
+
 function FooterComponent() {
   return (
     <footer>
       <hr />
-      <h5 className="d-flex justify-content-end mr-5 text-primary">
+      <h5 className="Footer-Text d-flex justify-content-end mr-5">
         Copyright © Designed and Made by David Félix
       </h5>
     </footer>
