9220fae3ccb5acc3a2c0170e3bb5d671c6ace474
David Felix <davidfelix1997@hotmail.com>
2020-03-22 00:38:08 +0000

    added links redirecting to linkedin, email, github

--- src/Views/AboutMe/index.jsx
+++ src/Views/AboutMe/index.jsx
@@ -44,27 +44,27 @@ class AboutMeView extends Component {
           </h3>
           <h2 className="AboutMe-ContactMeHere">{this.state.contactText}</h2>
           <div className="AboutMe-Social d-flex justify-content-around">
-            <Link target="_blank" to="https://www.linkedin.com/in/davidmcfelix/">
+            <a href="https://www.linkedin.com/in/davidmcfelix/">
               <Image
                 className="AboutMe-Social-img"
                 src="./images/social-icons/linkedin-icon.png"
                 alt="LinkedIn"
               />
-            </Link>
-            <Link to="https://github.com/DavidFelix8">
+            </a>
+            <a href="https://github.com/DavidFelix8">
               <Image
                 className="AboutMe-Social-img"
                 src="./images/social-icons/github-icon.png"
                 alt="Github"
               />
-            </Link>
-            <Link to={`mailto:davidfelix1997@hotmail.com`} target="_top">
+            </a>
+            <a href={`mailto:davidmcfelix@gmail.com`} target="_top">
               <Image
                 className="AboutMe-Social-img"
                 src="./images/social-icons/email-icon.png"
                 alt="Email"
               />
-            </Link>
+            </a>
           </div>
           <div>
             <button className="AboutMe-btn-ArrowDown" onClick={this.updateContent}>
